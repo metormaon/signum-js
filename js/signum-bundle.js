3f7ae3ede0764bd350e4e76c568c6e05cfda9c6b
metormaon <metormaon@users.noreply.github.com>
2020-05-03 22:58:15 +0000

    Github Action: Auto Minified JS and CSS files

--- js/signum-bundle.js
+++ js/signum-bundle.js
@@ -2153,7 +2153,7 @@ const loginConstraints = {
         numericality: {
             onlyInteger: true,
             greaterThan: 0,
-            lessThanOrEqualTo: 10
+            lessThanOrEqualTo: 50
         }
     },
     "hashcash.serverString": {
