e69204f7d7eb5cc3b746f7a19fa68b49e5589fe3
Noam Rotem <noam.rotem@thomsonreuters.com>
2020-05-08 09:39:45 +0300

    #27 - It's not working. Leaving it aside.

--- browser/browser-tests.js
+++ browser/browser-tests.js
@@ -5,15 +5,17 @@ describe("submitLogin", function() {
     it("should fail without username", async function () {
         let driver = await new Builder()
             .forBrowser('chrome')
-            .setChromeOptions(new chrome.Options().headless().windowSize({width: 1920, height: 1080}))
+            // .setChromeOptions(new chrome.Options().headless().windowSize({width: 1920, height: 1080}))
             .build();
 
         try {
             await driver.get("file://" + __dirname + "test.html");
 
             await expectAsync(
-                driver.executeScript("window.executeLogin();")
-            ).toBeRejectedWithError("Username is null or empty");
+                driver.wait( driver => {
+                    return driver.executeScript("executeLogin();")
+                }, 1000)
+            ).toBeRejectedWith("Username is null or empty");
         } finally {
             await driver.quit();
         }
